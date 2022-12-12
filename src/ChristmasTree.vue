8e4205d9eeda9e44d89a04ca77c5976cab8f4df8
Ezequiel Kohon <ezequiel.kohon@zaelot.com>
2022-12-12 17:34:08 -0300

    finished

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <div class="flex flex-row justify-center">
-      <!-- This div is styled for the tree sections, but it's not complete -->
-      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
-    </div>
+    <slot v-if="array.length === 1" v-bind:item="array[0]">
+      <div class="flex flex-row justify-center">
+        <!-- This div is styled for the tree sections, but it's not complete -->
+        <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+      </div>
+    </slot>
+
+    <ChristmasTree v-else :size="size - 1">
+      <template v-slot="{ item }">
+        <div class="flex flex-row justify-center">
+          <!-- This div is styled for the tree sections, but it's not complete -->
+          <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
+            v-for="n in size" />
+        </div>
+        <slot v-bind:item="item" />
+      </template>
+    </ChristmasTree>
   </div>
 </template>
 
-<script setup lang="ts">
-withDefaults(
-  defineProps<{
-    size: number
-  }>(),
-  {
-    size: 1,
-  }
-)
+<script setup>
+const props = defineProps({
+  size: Number
+})
+
+const array = Array.from(Array(props.size + 1).keys(), item => item + 1);
 </script>
